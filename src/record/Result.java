b9325741340778db6578aa04f90492399fbf80ff
Shiivu18 <shokdarshivum@gmail.com>
2026-04-23 08:00:35 +0530

    fixing student class

--- src/record/Result.java
+++ src/record/Result.java
@@ -1,41 +1,33 @@
 package record;
 
 public class Result {
-	
-	private int studentId;
-	private String subject;
-	private int marks;
-	@Override
-	public String toString() {
-		return "result [studentId=" + studentId + ", subject=" + subject + ", marks=" + marks + "]";
-	}
-	public int getStudentId() {
-		return studentId;
-	}
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
-	public String getSubject() {
-		return subject;
-	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	public int getMarks() {
-		return marks;
-	}
-	public void setMarks(int marks) {
-		this.marks = marks;
-	}
-	public Result(int studentId, String subject, int marks) {
-		super();
-		this.studentId = studentId;
-		this.subject = subject;
-		this.marks = marks;
-	}
-	
-	
-	
-	
 
+    private int studentId;
+    private String subject;
+    private int marks;
+
+    public Result(int studentId, String subject, int marks) {
+        this.studentId = studentId;
+        this.subject = subject;
+        this.marks = marks;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Result [studentId=" + studentId +
+                ", subject=" + subject +
+                ", marks=" + marks + "]";
+    }
 }
